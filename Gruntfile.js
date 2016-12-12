366ee79e37dad92022ecb76e36f4b16be67fe902
Francesco Zaia <francesco.zaia@gmail.com>
2016-12-12 10:48:08 +0100

    Moving around stuff

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        shopify: {
+            options: {
+                api_key: process.env.THEMEKIT_APIKEY,
+                password: process.env.THEMEKIT_PASSWORD,
+                url: process.env.THEMEKIT_STORE,
+                theme: process.env.THEMEKIT_THEME_ID,
+                base: "shop/"
+            }
+        },
+        watch: {
+            shopify: {
+                files: ["shop/**"],
+                tasks: ["shopify"]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shopify');
+    grunt.registerTask('default', 'watch:shopify');
+}
